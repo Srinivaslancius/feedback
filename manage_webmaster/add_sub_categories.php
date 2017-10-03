142e51f5acd18a1299837dae6ee12d3787b79079
srinivaslancuius <srinivas@lanciussolutions.com>
2017-10-03 18:51:08 +0530

    changes in files

--- manage_webmaster/add_sub_categories.php
+++ manage_webmaster/add_sub_categories.php
@@ -1,37 +1,24 @@
 <?php include_once 'admin_includes/main_header.php'; ?>
-
-<?php  if (!isset($_POST['submit']))  {
-          //If fail
-          echo "fail";
-        } else  {
-            //If success
-            $category_id = $_POST['category_id'];
-            $sub_category_name = $_POST['sub_category_name'];
-            $fileToUpload = $_FILES["fileToUpload"]["name"];
-            $status = $_POST['status'];
-            $feedbackOpt = implode(',',$_POST['feedback_options']);
-            
-            if($fileToUpload!='') {
-
-                $target_dir = "../uploads/sub_category_images/";
-                $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-                $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                    $sql = "INSERT INTO `sub_categories` (`category_id`, `sub_category_name`,`sub_category_image`, `status`,`subcat_feedback_options`) VALUES ('$category_id','$sub_category_name', '$fileToUpload','$status','$feedbackOpt')";
-                    if($conn->query($sql) === TRUE){
-                       echo "<script type='text/javascript'>window.location='sub_categories.php?msg=success'</script>";
-                    } else {
-                       echo "<script type='text/javascript'>window.location='sub_categories.php?msg=fail'</script>";
-                    }
-                    //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-                } else {
-                    echo "Sorry, there was an error uploading your file.";
-                }
-            }   
-        }
+<?php  
+if (!isset($_POST['submit']))  {
+  //If fail
+  echo "fail";
+}else  {
+  //If success
+  $category_id = $_POST['category_id'];
+  $sub_category_name = $_POST['sub_category_name'];
+  $status = $_POST['status'];
+  $feedbackOpt = implode(',',$_POST['feedback_options']);
+  
+  $sql = "INSERT INTO `sub_categories` (`category_id`, `sub_category_name`, `status`,`subcat_feedback_options`) VALUES ('$category_id','$sub_category_name', '$status','$feedbackOpt')";
+  if($conn->query($sql) === TRUE){
+    echo "<script type='text/javascript'>window.location='sub_categories.php?msg=success'</script>";
+  }else {
+    echo "<script type='text/javascript'>window.location='sub_categories.php?msg=fail'</script>";
+  }
+}
 ?>
-		<div class="site-content">
+      <div class="site-content">
         <div class="panel panel-default">
           <div class="panel-heading">
             <h3 class="m-y-0"> Sub Categories</h3>
@@ -56,22 +43,14 @@
                     <input type="text" class="form-control" id="form-control-2" name="sub_category_name" placeholder="Sub Category Name" data-error="Please enter Category Name." required>
                     <div class="help-block with-errors"></div>
                   </div>
-				          <div class="form-group">
-                    <label for="form-control-4" class="control-label">Sub Category Image</label>
-                    <img id="output" height="100" width="100"/>
-                    <label class="btn btn-default file-upload-btn">
-						          Choose file...
-                        <input id="form-control-22" class="file-upload-input" type="file" accept="image/*" name="fileToUpload" id="fileToUpload"  onchange="loadFile(event)"  multiple="multiple" required >
-                      </label>
-                  </div>
-                  <?php $getfeedbackOpt = getDataFromTables('feedback_options','0',$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);?>
+                  <?php $getStatus = getDataFromTables('feedback_options','0',$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);?>
                   <div class="form-group">
-                    <label for="form-control-2" class="control-label">Feedback Options : </label><br />
-                    <?php while ($row = $getfeedbackOpt->fetch_assoc()) { ?>
-                    <input type="checkbox" value="<?php echo $row['id']; ?>" name="feedback_options[]"> <?php echo $row['feedback_option']; ?> &nbsp;&nbsp;
+                    <label for="form-control-2" class="control-label">Feedback option</label>
+                    <?php while ($row = $getStatus->fetch_assoc()) { ?>
+                    <input type="checkbox" value="<?php echo $row['id']; ?>" name="feedback_options[]"><?php echo $row['feedback_option']; ?>
                     <?php } ?>
                   </div>
-				          <?php $getStatus = getDataFromTables('user_status',$status=NULL,$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);?>
+                  <?php $getStatus = getDataFromTables('user_status',$status=NULL,$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);?>
                   <div class="form-group">
                     <label for="form-control-3" class="control-label">Choose your status</label>
                     <select id="form-control-3" name="status" class="custom-select" data-error="This field is required." required>
@@ -93,17 +72,3 @@
       </div>
       <?php include_once 'admin_includes/footer.php'; ?>
       <script src="js/tables-datatables.min.js"></script>
-
-      
-       
-
-      
-      
-
-      
-
-
-
-
-
-
--- manage_webmaster/edit_sub_categories.php
+++ manage_webmaster/edit_sub_categories.php
@@ -1,43 +1,24 @@
 <?php include_once 'admin_includes/main_header.php'; ?>
-<?php
+<?php  
 $id = $_GET['bid'];
- if (!isset($_POST['submit']))  {
-            echo "fail";
-    } else  {
-            $category_id = $_POST['category_id'];
-            $status = $_POST['status'];
-            $sub_category_name = $_POST['sub_category_name'];
-            $feedbackOpt = implode(',',$_POST['feedback_options']);
-
-            if($_FILES["fileToUpload"]["name"]!='') {
-              $fileToUpload = $_FILES["fileToUpload"]["name"];
-              $target_dir = "../uploads/sub_category_images/";
-              $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-              $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-              $getImgUnlink = getImageUnlink('sub_category_image','sub_categories','id',$id,$target_dir);
-                //Send parameters for img val,tablename,clause,id,imgpath for image ubnlink from folder
-              if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                    $sql = "UPDATE `sub_categories` SET category_id = '$category_id', sub_category_name = '$sub_category_name', sub_category_image = '$fileToUpload', status='$status',subcat_feedback_options='$feedbackOpt' WHERE id = '$id' ";
-                    if($conn->query($sql) === TRUE){
-                       echo "<script type='text/javascript'>window.location='sub_categories.php?msg=success'</script>";
-                    } else {
-                       echo "<script type='text/javascript'>window.location='sub_categories.php?msg=fail'</script>";
-                    }
-                    //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-                } else {
-                    echo "Sorry, there was an error uploading your file.";
-                }
-            }  else {
-                $sql = "UPDATE `sub_categories` SET category_id = '$category_id', sub_category_name = '$sub_category_name', status='$status',subcat_feedback_options='$feedbackOpt' WHERE id = '$id' ";
-                if($conn->query($sql) === TRUE){
-                   echo "<script type='text/javascript'>window.location='sub_categories.php?msg=success'</script>";
-                } else {
-                   echo "<script type='text/javascript'>window.location='sub_categories.php?msg=fail'</script>";
-                }
-            }
-          }
+if (!isset($_POST['submit'])) {
+      //If fail
+        echo "fail";
+    } else {
+    //If success            
+    $category_id = $_POST['category_id'];
+    $status = $_POST['status'];
+    $sub_category_name = $_POST['sub_category_name'];
+    $feedbackOpt = implode(',',$_POST['feedback_options']);
+        $sql = "UPDATE `sub_categories` SET category_id = '$category_id', sub_category_name = '$sub_category_name',  status='$status',subcat_feedback_options='$feedbackOpt' WHERE id = '$id' ";
+        if($conn->query($sql) === TRUE){
+           echo "<script type='text/javascript'>window.location='sub_categories.php?msg=success'</script>";
+        } else {
+           echo "<script type='text/javascript'>window.location='sub_categories.php?msg=fail'</script>";
+        }
+    }   
 ?>
-<?php $getSubCategoriesData = getDataFromTables('sub_categories',$status=NULL,'id',$id,$activeStatus=NULL,$activeTop=NULL);
+      <?php $getSubCategoriesData = getDataFromTables('sub_categories',$status=NULL,'id',$id,$activeStatus=NULL,$activeTop=NULL);
 $getSubCategories = $getSubCategoriesData->fetch_assoc();
 $getCategories = getDataFromTables('categories','0',$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);
  ?>
@@ -64,14 +45,6 @@ $getCategories = getDataFromTables('categories','0',$clause=NULL,$id=NULL,$activ
                     <label for="form-control-2" class="control-label">Sub Category Name</label>
                     <input type="text" class="form-control" id="form-control-2" name="sub_category_name" required value="<?php echo $getSubCategories['sub_category_name'];?>">
                     <div class="help-block with-errors"></div>
-                  </div>
-                  <div class="form-group">
-                    <label for="form-control-4" class="control-label">Sub Category Image</label>
-                    <img src="<?php echo $base_url . 'uploads/sub_category_images/'.$getSubCategories['sub_category_image'] ?>"  id="output" height="100" width="100"/>
-                    <label class="btn btn-default file-upload-btn">
-                        Choose file...
-                        <input id="form-control-22" class="file-upload-input" type="file" accept="image/*" name="fileToUpload" id="fileToUpload"  onchange="loadFile(event)"  multiple="multiple" >
-                      </label>
                   </div>
 
                   <?php  
@@ -108,4 +81,3 @@ $getCategories = getDataFromTables('categories','0',$clause=NULL,$id=NULL,$activ
       </div>
       <?php include_once 'admin_includes/footer.php'; ?>
    <script src="js/tables-datatables.min.js"></script>
-
