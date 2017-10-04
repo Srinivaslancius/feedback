59a584e7329ee1813cb26d6a7b3c47bd14d8485a
srinivaslancius <srinivas@lanciussolutions.com>
2017-10-04 17:22:33 +0530

    Changed dashboard page and side navigation page

--- client_manage_admin/dashboard.php
+++ client_manage_admin/dashboard.php
@@ -1,33 +1,7 @@
 <?php include_once 'admin_includes/main_header.php'; ?>
       <div class="site-content">
         <div class="row">
-          <div class="col-md-4 col-sm-5">
-            <div class="widget widget-tile-2 bg-primary m-b-30">
-              <div class="wt-content p-a-20 p-b-50">
-                <div class="wt-title">Super Admin
-                  <span class="t-caret text-success">
-                    <i class="zmdi zmdi-caret-up"></i>
-                  </span>
-                </div>
-                <div class="wt-number"><?php echo getRowsCount('users')?></div>
-              </div>
-              <div class="wt-icon">
-                <i class="zmdi zmdi-accounts"></i>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 col-sm-5">
-            <div class="widget widget-tile-2 bg-warning m-b-30">
-              <div class="wt-content p-a-20 p-b-50">
-                <div class="wt-title">Client Admin</div>
-                <div class="wt-number"><?php echo getRowsCount('client_admin_users')?></div>
-              </div>
-              <div class="wt-icon">
-                <i class="zmdi zmdi-accounts"></i>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 col-sm-5">
+          <div class="col-md-3 col-sm-3">
             <div class="widget widget-tile-2 bg-danger m-b-30">
               <div class="wt-content p-a-20 p-b-50">
                 <div class="wt-title">Supervisor Admin</div>
@@ -39,7 +13,7 @@
             </div>
           </div>
         
-          <div class="col-md-4 col-sm-4">
+          <div class="col-md-3 col-sm-3">
             <div class="widget widget-tile-2 bg-danger m-b-30">
               <div class="wt-content p-a-20 p-b-50">
                 <div class="wt-title">Good</div>
@@ -50,7 +24,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md-4 col-sm-4">
+          <div class="col-md-3 col-sm-3">
             <div class="widget widget-tile-2 bg-primary m-b-30">
               <div class="wt-content p-a-20 p-b-50">
                 <div class="wt-title">Average</div>
@@ -61,7 +35,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md-4 col-sm-4">
+          <div class="col-md-3 col-sm-3">
             <div class="widget widget-tile-2 bg-warning m-b-30">
               <div class="wt-content p-a-20 p-b-50">
                 <div class="wt-title">Poor</div>
@@ -73,6 +47,61 @@
             </div>
           </div>
         </div>
+        <div>
+        <?php
+          $client_name = $_SESSION['client_admin_user_name'];
+          $sql = "SELECT * FROM client_admin_users WHERE client_name = '$client_name' AND status=0";
+          $result = $conn->query($sql);
+          $row = $result->fetch_assoc();
+        ?>
+        <?php if ($row) { ?>
+        <center><table>
+          <h1 style="text-align:center;">Client Information</h1>
+          <tr>
+            <td>Name: </td>
+            <td><?php echo $row['client_name'];?></td>
+          </tr>
+          <tr>
+            <td>Email: </td>
+            <td><?php echo $row['client_email'];?></td>
+          </tr>
+          <tr>
+            <td>Mobile: </td>
+            <td><?php echo $row['client_mobile'];?></td>
+          </tr>
+          <tr>
+            <td>Country: </td>
+            <td><?php $getCountryData =  getDataFromTables('lkp_countries',$status=NULL,'id',$row['client_country_id'],$activeStatus=NULL,$activeTop=NULL); $country = $getCountryData->fetch_assoc(); echo $country['country_name']?></td>
+          </tr>
+          <tr>
+            <td>State: </td>
+            <td><?php $getStateData =  getDataFromTables('lkp_states',$status=NULL,'id',$row['client_state_id'],$activeStatus=NULL,$activeTop=NULL); $state = $getStateData->fetch_assoc(); echo $state['state_name']?></td>
+          </tr>
+          <tr>
+            <td>City: </td>
+            <td><?php $getCityData =  getDataFromTables('lkp_cities',$status=NULL,'id',$row['client_city_id'],$activeStatus=NULL,$activeTop=NULL); $city = $getCityData->fetch_assoc(); echo $city['city_name']?></td>
+          </tr>
+          <tr>
+        </table></center>
+        <?php } ?>
+        </div>
+
+        <style>
+          table {
+              font-family: arial, sans-serif;
+              border-collapse: collapse;
+              width: 50%;
+          }
+
+          td {
+              text-align: center;
+              padding: 8px;
+          }
+
+          tr:nth-child(even) {
+              background-color: #dddddd;
+          }
+          </style>
 
        <!--  <div class="col-md-6 m-b-30">
           <h4 class="m-t-0 m-b-30">Pie chart</h4>
